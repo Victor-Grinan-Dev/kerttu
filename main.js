2b7790fd2aef9b1afa2922564c136d027badbd71
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-07-20 18:40:49 +0300

    tutorials caption

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 const tutorialsData = [
-  "https://www.youtube.com/embed/y3r33AZhUvw",
-  "https://www.youtube.com/embed/W6vx6dqCZXg",
-  "https://www.youtube.com/embed/A77Xi2pE7Lg",
-  "https://www.youtube.com/embed/5VRkIaAkG2k",
+  {
+    source: "https://www.youtube.com/embed/y3r33AZhUvw",
+    caption: "Caption for tutorial 1",
+  },
+  {
+    source: "https://www.youtube.com/embed/W6vx6dqCZXg",
+    caption: "Caption for tutorial 2",
+  },
+  {
+    source: "https://www.youtube.com/embed/A77Xi2pE7Lg",
+    caption: "Caption for tutorial 3",
+  },
+  {
+    source: "https://www.youtube.com/embed/5VRkIaAkG2k",
+    caption: "Caption for tutorial 4",
+  },
 ];
 const tutorialsSection = document.querySelector(".tutorials");
 const copyright = document.querySelector(".footer__copyright");
 
 tutorialsData.forEach((tutorial) => {
   tutorialsSection.innerHTML += `
-    <iframe
+  <div>
+  <iframe
     width="280"
     height="150"
-    src=${tutorial}
+    src=${tutorial.source}
     title="YouTube video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     allowfullscreen
     class="video"
-  ></iframe>`;
+  ></iframe>
+  <p>${tutorial.caption}</p>
+  </div>
+  `;
 });
 
 /* copyrights */
